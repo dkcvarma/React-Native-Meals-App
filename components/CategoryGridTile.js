e9dbb34b5086e37aeaa90dc1b0a2aa4ef8aad4f9
Krishna Chaitanya <DatlaKrishnaChaitanya.Varma@mrcooper.com>
2021-02-25 23:34:42 +0530

    Fixing the Shadows

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -11,7 +11,7 @@ import {
 const CategoryGridTile = props => {
   let TouchableCmp = TouchableOpacity;
 
-  if (Platform.OS === 'android' && Platform.Version >= 21 ) {
+  if ( Platform.OS === 'android' && Platform.Version >= 21 ) {
     TouchableCmp = TouchableNativeFeedback;
   }
   return (
@@ -31,16 +31,16 @@ const styles = StyleSheet.create({
     margin: 15,
     height: 150,
     borderRadius: 10,
-    overflow: 'hidden'
+    overflow: Platform.OS === 'android' && Platform.Version >= 21 ? 'hidden' : 'visible',
+    elevation: 5
   },
   container: {
     flex: 1,
     borderRadius: 10,
     shadowColor: 'black',
-    shadowOffset: 0.26,
+    shadowOpacity: 0.26,
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 10,
-    elevation: 3,
     padding: 15,
     justifyContent: 'flex-end',
     alignItems: 'flex-end'
