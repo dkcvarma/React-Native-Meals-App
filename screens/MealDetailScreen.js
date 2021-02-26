c18511274f49acd686a9657291b744db130e6fa5
Krishna Chaitanya <DatlaKrishnaChaitanya.Varma@mrcooper.com>
2021-02-26 23:36:57 +0530

    Adding Meal Detail Screen Content

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { ScrollView, View, Image, Text, Button, StyleSheet } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons'
 
 import { MEALS } from '../data/dummy-data';
-import HeaderButton from '../components/HeaderButton'
+import HeaderButton from '../components/HeaderButton';
+import DefaultText from '../components/DefaultText';
+
+const ListItem = props => {
+  return (
+    <View style={styles.ListItem}>
+      <DefaultText>{props.children}</DefaultText>
+    </View>
+  );
+}
 
 const MealDetailScreen = props => {
   const mealId = props.navigation.getParam('mealId');
@@ -11,12 +20,22 @@ const MealDetailScreen = props => {
   const selectedMeal = MEALS.find(meal => meal.id === mealId);
 
   return (
-    <View style={styles.screen}>
-      <Text>The Meal Detail Screen!</Text>
-      <Button title="Go Back to Categories" onPress={() => {
-        props.navigation.popToTop();
-      }} />
-    </View>
+    <ScrollView>
+      <Image source={{uri: selectedMeal.imageUrl}} style={styles.image} />
+      <View style={styles.details}>
+        <DefaultText>{selectedMeal.duration}m</DefaultText>
+        <DefaultText>{selectedMeal.complexity.toUpperCase()}</DefaultText>
+        <DefaultText>{selectedMeal.affordability.toUpperCase()}</DefaultText>
+      </View>
+      <Text style={styles.title}>Ingredients</Text>
+      {selectedMeal.ingredients.map(ingredient => (
+        <ListItem key={ingredient}>{ingredient}</ListItem>
+      ))}
+      <Text style={styles.title}>Steps</Text>
+      {selectedMeal.steps.map(step => (
+        <ListItem key={step}>{step}</ListItem>
+      ))}
+    </ScrollView>
   );
 };
 
@@ -38,10 +57,26 @@ MealDetailScreen.navigationOptions = (navigationData) => {
 };
 
 const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+  image: {
+    width: '100%',
+    height: 200
+  },
+  details: {
+    flexDirection: 'row',
+    padding: 15,
+    justifyContent: 'space-around'
+  },
+  title: {
+    fontFamily: 'open-sans-bold',
+    fontSize: 22,
+    textAlign: 'center'
+  },
+  ListItem: {
+    marginVertical: 10,
+    marginHorizontal: 20,
+    borderColor: '#CCC',
+    borderWidth: 1,
+    padding: 10
   }
 });
 
