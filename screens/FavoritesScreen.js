55f181d76529eda1822297bca7f4e76c502eaf0c
Krishna Chaitanya <DatlaKrishnaChaitanya.Varma@mrcooper.com>
2021-02-27 13:08:28 +0530

    Default Favorites Screen Text

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { View, StyleSheet } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import { useSelector } from 'react-redux';
 
 import HeaderButton from '../components/HeaderButton'
 import MealList from '../components/MealList';
-import { MEALS } from '../data/dummy-data';
+import DefaultText from '../components/DefaultText';
 
 const FavioritesScreen = props => {
   const favMeals = useSelector(state => state.meals.favoriteMeals);
+
+  if (favMeals.length === 0 || !favMeals) {
+    return (
+      <View style={styles.content}>
+        <DefaultText>No favorite meals found. Start adding some!</DefaultText>
+      </View>
+    );
+  }
 
   return <MealList listData={favMeals} navigation={props.navigation} />;
 };
@@ -28,5 +37,13 @@ FavioritesScreen.navigationOptions = navData => {
     )
   };
 }
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default FavioritesScreen;
