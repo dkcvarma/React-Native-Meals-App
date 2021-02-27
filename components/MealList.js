41e58530b6c2d9613414b912772ea31770aed44c
Krishna Chaitanya <DatlaKrishnaChaitanya.Varma@mrcooper.com>
2021-02-27 13:02:46 +0530

    Switching the Favorites Icon

--- components/MealList.js
+++ components/MealList.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { View, FlatList, StyleSheet } from 'react-native';
+import { useSelector } from 'react-redux';
 
 import MealItem from './MealItem';
 
 const MealList = props => {
+  const favoriteMeals = useSelector(state => state.meals.favoriteMeals);
+
   const renderMealItem = itemData => {
+    const isFavorite = favoriteMeals.some(meal => meal.id === itemData.item.id);
     return (
       <MealItem
         title={itemData.item.title}
@@ -13,9 +17,11 @@ const MealList = props => {
         complexity={itemData.item.complexity}
         affordability={itemData.item.affordability}
         onSelectMeal = {() => {
-          props.navigation.navigate(
-            'MealDetail',
-            {mealId: itemData.item.id, mealTitle: itemData.item.title});
+          props.navigation.navigate('MealDetail', {
+            mealId: itemData.item.id,
+            mealTitle: itemData.item.title,
+            isFav: isFavorite,
+          });
         }}
       />
     );
